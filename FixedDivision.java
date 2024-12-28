0f73ac1bc262ecaf396c57b98022de8ef72ddcc6
BugHunter-X <askzenai@gmail.com>
2024-12-28 14:24:05 +0100

    Add FixedDivision.java file

---
+++ FixedDivision.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 0;
+        if (y != 0) {
+            System.out.println(x / y);
+        } else {
+            System.out.println("Cannot divide by zero.");
+        }
+    }
+}
