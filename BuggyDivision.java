b94c061832882d6f86e826044e241815a82943ba
BugHunter-X <askzenai@gmail.com>
2024-12-28 14:24:04 +0100

    Add BuggyDivision.java file

---
+++ BuggyDivision.java
@@ -0,0 +1,7 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 0;
+        System.out.println(x / y); // This line causes an ArithmeticException
+    }
+}
